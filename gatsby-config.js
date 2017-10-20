be8ab496314526d644ad7bf156e21402a5463ea4
Michael Delaney <michael@fusepilot.com>
2017-10-19 21:43:13 -0700

    filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-react-next'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-react-next',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'content',
+        path: `${__dirname}/content`,
+      },
+    },
+  ],
 }
